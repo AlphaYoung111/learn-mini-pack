cdd6ad08d5e2f0ad36723fe965406a599b4d1853
AlphaYoung111 <729346015@qq.com>
2022-01-13 23:34:39 +0800

    feat:初始化依赖

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import fs from 'fs'
+
 function createAssets () {
+  // 获取文件内容
+  const result = fs.readFileSync('./example/main.js',{
+    encoding:'utf8'
+  })
+
+  console.log(result);
+
+  // 获取依赖关系
   return {
     
   }
 }
+
+createAssets()
