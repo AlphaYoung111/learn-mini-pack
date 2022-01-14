83f099d4e61784bb2b9d935289b1f57772a0e4bb
AlphaYoung111 <729346015@qq.com>
2022-01-14 22:36:53 +0800

    add:babel依赖

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import fs from 'fs'
+import parser from '@babel/parser'
+import traverse from '@babel/traverse'
 
-function createAssets () {
+function createAssets() {
   // 获取文件内容
-  const result = fs.readFileSync('./example/main.js',{
-    encoding:'utf8'
+  const result = fs.readFileSync('./example/main.js', {
+    encoding: 'utf8',
   })
 
-  console.log(result);
+  console.log(result)
 
   // 获取依赖关系
-  return {
-    
-  }
+
+  const ast = parser.parse(result, {
+    sourceType: 'module',
+  })
+
+  // 遍历树
+  traverse.default(ast, {
+    // 当访问到这个节点的时候就会执行这个函数
+    ImportDeclaration({node }) {
+      console.log(node.source.value)
+    },
+  })
+
+  console.log(ast)
+
+  return {}
 }
 
 createAssets()
